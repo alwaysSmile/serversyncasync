7315467fd9a48dab7c374ccf477c2998622f31f1
alwaysSmile <sjuice@mail.ru>
2017-03-16 16:21:30 +0300

    sync and async

---
+++ serverSync.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+    var info;
+
+    if (req.url == '/') {//1. При получении такого запроса на url == '/'
+        try {
+            //Вместо запроса fs.readFileSync('index.html'); может быть запрос к базе данным
+            info = fs.readFileSync('index.html');//2. Считывается файл index.html
+        } catch (err) {
+            console.error(err);
+            res.statusCode = 500;
+            res.end("На сервере произошла ошибка!")//3. Выводится посетитель
+            return;
+        }
+
+        res.end(info);
+    } else {/* 404 */}
+}).listen(3000);
+
+//Этот файл НЕ актуален в серверном окружении, когда нужно делать много вещей одновременно
